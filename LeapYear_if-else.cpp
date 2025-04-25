cce8aea2d084fc48e6eab85b97219b52a42734a8
mdsifatkhan99 <official.sifat99@gmail.com>
2025-04-25 16:22:48 +0600

    Add files via upload

---
+++ LeapYear_if-else.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+    int year;
+
+    cout << "Enter a year to check leap year: "<<endl;
+    cin >> year;
+
+    if(year%100 !=0 && year%4==0 || year%400==0)
+        cout <<year<<" is a leap year."<<endl;
+    else
+        cout<<year<<" is not a leap year."<<endl;
+        return 0;
+
+}
