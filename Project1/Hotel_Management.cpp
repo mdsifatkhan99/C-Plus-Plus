17000efea7a4195d451d2343dc141add5215c0f1
MD SIFAT KHAN <official.sifat99@gmail.com>
2025-05-15 16:21:16 +0600

    Update Hotel_Management.cpp

--- Project1/Hotel_Management.cpp
+++ Project1/Hotel_Management.cpp
@@ -7,7 +7,7 @@ int main ()
     int choice;
 
     //Quantity
-    int Qrooms=0, Qpasta=0, Qburger=0, Qnoodles, Qshake=0, Qchicken=0;
+    int Qrooms=0, Qpasta=0, Qburger=0, Qnoodles=0, Qshake=0, Qchicken=0;
 
     //food items sold
    int Srooms=0, Spasta=0, Sburger=0, Snoodles=0, Sshake=0, Schicken=0;
